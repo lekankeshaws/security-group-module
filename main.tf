8cbf691318baedb627ebc8ecef194404b8c25931
lekankeshaws <lekankeshaws@gmail.com>
2023-03-06 00:13:42 -0500

    pushing updated

--- main.tf
+++ main.tf
@@ -65,12 +65,3 @@ resource "aws_security_group_rule" "ingress_rule_database" {
   security_group_id        = aws_security_group.security["database_sg"].id
 }
 
-# resource "aws_security_group_rule" "egress_rule" {
-#   count = length(var.from_port)
-#   type              = "egress"
-#   to_port           = element(var.port, count.index)
-#   protocol          = var.protocol
-#   cidr_blocks       = var.cidr_block
-#   from_port         = element(var.port, count.index)
-#   security_group_id = element(aws_security_group.security[*].id, count.index)
-# }
